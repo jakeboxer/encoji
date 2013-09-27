ee681c2f38f4d01edb4965c50db8fa352397ab07
Jake Boxer <jake@github.com>
2013-09-27 15:38:01 -0700

    Add specs for multi-codepoint emoji encoding

--- spec/encoji_spec.rb
+++ spec/encoji_spec.rb
@@ -9,6 +9,16 @@ describe Encoji do
     it 'converts a thumbsup emoji to cheat sheet form' do
       expect(Encoji.to_cheat_sheet(emoji(:thumbsup))).to eql(':thumbsup:')
     end
+
+    it "converts multi-codepoint emoji to cheat sheet form" do
+      text = "i like #{emoji(:pound)}hashtags and #{emoji(:one)}direction they are the #{emoji(:pound)}#{emoji(:one)} band. end with #{emoji(:nine)}"
+      expect(Encoji.to_cheat_sheet(text)).to eql('i like :pound:hashtags and :one:direction they are the :pound::one: band. end with :nine:')
+    end
+
+    it "preserves ASCII characters that match the first codepoint in multi-codepoint emoji" do
+      text = "i like #hashtags and 1direction they are the #1 band. end with 9"
+      expect(Encoji.to_cheat_sheet(text)).to eql(text)
+    end
   end
 
   describe 'to_emoji' do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,10 @@ module Helpers
   def emoji(name)
     @emoji ||= {
       :smile    => "\u{1f604}",
-      :thumbsup => "\u{1f44d}"
+      :thumbsup => "\u{1f44d}",
+      :pound    => "\u{1f4b7}",
+      :one      => "\u{0031 20e3}",
+      :nine     => "\u{0039 20e3}"
     }
 
     @emoji[name]
